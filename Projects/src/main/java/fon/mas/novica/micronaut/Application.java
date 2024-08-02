14f18fc20ec98341d003d1bd9ef6fda9a8165456
Novke <novke.brate@gmail.com>
2024-08-02 15:15:21 +0200

    properties 4 consul

--- Projects/src/main/java/fon/mas/novica/micronaut/Application.java
+++ Projects/src/main/java/fon/mas/novica/micronaut/Application.java
@@ -1,10 +1,31 @@
 package fon.mas.novica.micronaut;
 
+import io.micronaut.context.annotation.Value;
+import io.micronaut.context.env.Environment;
+import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.runtime.event.annotation.EventListener;
+import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    @Inject
+    Environment environment;
+    @Value("${config.source}")
+    String configSource;
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+    }
+
+
+    @EventListener
+    void check(StartupEvent event){
+        log.info("======= CHECKING CONFIG SERVER =======");
+        log.info("Config source: {}", configSource);
+        log.info("======================================");
     }
 }
